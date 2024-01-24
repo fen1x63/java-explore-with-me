ad31a649de024d704569f5ca86a6217bc8aa0e25
Mikhail <fen1xdota@yandex.ru>
2024-01-24 13:34:35 +0300

    Правки аннотаций

--- main/src/main/java/ru/practicum/ewm/events/dto/EventAdminRequestDto.java
+++ main/src/main/java/ru/practicum/ewm/events/dto/EventAdminRequestDto.java
@@ -22,8 +22,8 @@ public class EventAdminRequestDto {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime rangeEnd;
     @PositiveOrZero
-    private Integer from;
+    private Integer from = 0;
     @Positive
-    private Integer size;
+    private Integer size = 10;
 
 }
