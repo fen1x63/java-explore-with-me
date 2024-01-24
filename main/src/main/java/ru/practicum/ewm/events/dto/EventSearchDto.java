c8eb1b63301504f279f8e43851ebc10eb34121cd
Mikhail <fen1xdota@yandex.ru>
2024-01-24 21:33:35 +0300

    Правки контроллера

--- main/src/main/java/ru/practicum/ewm/events/dto/EventSearchDto.java
+++ main/src/main/java/ru/practicum/ewm/events/dto/EventSearchDto.java
@@ -1,8 +1,13 @@
 package ru.practicum.ewm.events.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.springframework.format.annotation.DateTimeFormat;
+import ru.practicum.ewm.categories.dto.CategoryDto;
+import ru.practicum.ewm.events.model.State;
+import ru.practicum.ewm.locations.LocationDto;
+import ru.practicum.ewm.users.dto.UserShortDto;
 
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
@@ -20,18 +25,35 @@ import static ru.practicum.ewm.util.DateConstant.DATE_TIME_PATTERN;
 @FieldDefaults(level = AccessLevel.PUBLIC)
 public class EventSearchDto {
 
-    private String text = "";
-    private List<Long> categories;
-    private Boolean paid = false;
+    Long id;
+    String annotation;
+    CategoryDto category;
+    Long confirmedRequests;
+    @JsonFormat(pattern = DATE_TIME_PATTERN)
+    LocalDateTime createdOn;
+    String description;
+    @JsonFormat(pattern = DATE_TIME_PATTERN)
+    LocalDateTime eventDate;
+    UserShortDto initiator;
+    LocationDto location;
+    Integer participantLimit;
+    @JsonFormat(pattern = DATE_TIME_PATTERN)
+    LocalDateTime publishedOn;
+    Boolean requestModeration;
+    State state;
+    String title;
+    String text = "";
+    List<Long> categories;
+    Boolean paid = false;
     @DateTimeFormat(pattern = DATE_TIME_PATTERN)
-    private LocalDateTime rangeStart;
+    LocalDateTime rangeStart;
     @DateTimeFormat(pattern = DATE_TIME_PATTERN)
-    private LocalDateTime rangeEnd;
-    private Boolean onlyAvailable = false;
-    private String sort = "EVENT_DATE";
+    LocalDateTime rangeEnd;
+    Boolean onlyAvailable = false;
+    String sort = "EVENT_DATE";
     @PositiveOrZero
-    private Integer from = 0;
+    Integer from = 0;
     @Positive
-    private Integer size = 10;
+    Integer size = 10;
 
 }
